3e7e8e8969015b494a8831e39896b6cd2f49a5c3
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-08-02 18:07:33 +0530

    Phase 0

---
+++ Phase_0/app.py
@@ -0,0 +1,8 @@
+from flask import Flask # we are importing Flask from flask 
+
+app = Flask(__name__)  # we are making an object of the flask that represent our website
+
+@app.route("/") #this is our home page
+def home(): # we define a function that is connected to our home page
+    return "Hello Everyone ! This is my first flask app." # we return a string that is visible in our home page
+
